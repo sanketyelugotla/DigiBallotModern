9a3b6704f289b403ccb929c15ce4038d49269653
sanket <sanketyelugotla123@gmail.com>
2025-04-01 14:36:51 +0530

    added tootopli

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,7 +6,11 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
+        });
         console.log("MongoDB Connected...");
     } catch (err) {
         console.error(err.message);
